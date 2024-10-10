c605d8d846c33fa2eb32f51298b3822e92f96ed2
Cocoagoat <denis021299@gmail.com>
2024-10-11 00:46:38 +0300

    Made daily update mechanism failure-resistant

--- malstats/main/tasks.py
+++ malstats/main/tasks.py
@@ -7,7 +7,7 @@ from .modules.SeasonalStats import SeasonalStats
 from .modules.AnimeDB import AnimeDB
 from .modules.filenames import *
 from celery import shared_task, Task
-from .modules.general_utils import redis_cache_wrapper, determine_queue_cache_key, add_suffix_to_filename
+from .modules.general_utils import redis_cache_wrapper, determine_queue_cache_key, add_suffix_to_filename, log_exception_error
 from .modules.Errors import UserListFetchError
 from .modules.AffinityFinder import find_max_affinity
 from .modules.Model import add_image_urls_to_predictions
@@ -83,6 +83,7 @@ def get_user_seasonal_stats_task(username, site="MAL"):
 @shared_task(base=MainTask)
 @redis_cache_wrapper(timeout=CACHE_TIMEOUT)
 def get_user_recs_task(username, site="MAL"):
+    print(f"GPU available : {tf.test.is_gpu_available()}")
     recs_logger.info(f"Entering recommendations task for {site} user {username}")
     global model
     if not model:
@@ -127,26 +128,37 @@ def get_user_affs_task(username, site="MAL"):
         return {'error': "An unexpected error has occurred on our side. Please try again later.", 'status': 500}
 
 
-@shared_task
-def update_anime_db():
+@shared_task(name="main.tasks.daily_update")
+def daily_update():
+    daily_backup()
     try:
-        AnimeDB().generate_anime_DB(update=True)
-    except Exception as e:
-        logger.error(f"An unexpected error has occurred while updating the anime database. {str(e)}")
-
-
-@shared_task(bind=True, autoretry_for=(TimeLimitExceeded,), retry_kwargs={'max_retries': 10, 'countdown': 60},
-             soft_time_limit=6600, default_retry_delay=60, time_limit=7200, name='main.tasks.daily_update')
-def daily_update(self):
-    try:
-        daily_backup()
         AnimeDB(anime_database_updated_name).generate_anime_DB(update=True)
-        Tags().update_tags()
-    except Exception as e:
-        full_stack_trace = traceback.format_exc()  # Capture the full traceback as a string
-        logger.error(f"An unexpected error has occurred during the daily update. {str(e)}")
-        logger.error(full_stack_trace)
-        self.retry()
+    except Exception:
+        # Theoretically should never happen unless something's wrong with MAL itself.
+        log_message = f"Encountered a critical failure during AnimeDB daily update." \
+                      f" Manual intervention required."
+        log_exception_error(logger, log_message)
+        restore_previous_files()
+
+    try:
+        Tags().update_tags(update_from_scratch=False)
+    except Exception:
+        log_message = f"Graphs/Tags daily update failed. " \
+                      f"Restoring previous files and attempting update from scratch."
+        log_exception_error(logger, log_message)
+        restore_previous_files(exclude_anime_db=True)
+
+        # Most of the time, updating from scratch (using the original entry_tags_dict, shows_tags_dict
+        # etc files) helps if something goes wrong.
+        try:
+            Tags().update_tags(update_from_scratch=True)
+        except Exception:
+            # If update still fails, a new show must have caused the issue and we need to manually
+            # check what's wrong.
+            log_message = f"Encountered a critical failure during Graphs/Tags daily update."\
+                          f" Manual intervention required."
+            log_exception_error(logger, log_message)
+            restore_previous_files()
 
 
 def daily_backup():
@@ -164,3 +176,19 @@ def daily_backup():
             shutil.copy(filename, add_suffix_to_filename(filename, "prev"))
         except FileNotFoundError:
             continue  # First time updating, -U files haven't been created yet
+
+
+def restore_previous_files(exclude_anime_db=False):
+    filenames_to_backup = [entry_tags_updated_filename,
+                           entry_tags_nls_updated_filename, shows_tags_updated_filename,
+                           shows_tags_nls_updated_filename, graphs_dict_updated_filename,
+                           graphs_dict_nls_updated_filename]
+
+    if not exclude_anime_db:
+        shutil.copy(add_suffix_to_filename(anime_database_updated_name,
+                                           "prev_before_update"), anime_database_updated_name)
+    for filename in filenames_to_backup:
+        try:
+            shutil.copy(add_suffix_to_filename(filename, "prev"), filename)
+        except FileNotFoundError:
+            continue  # "Prev" wasn't created during the failed update
