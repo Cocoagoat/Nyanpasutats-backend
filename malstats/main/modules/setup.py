c81b4b1fa6ff0b740f0ae37fc8e453a8f4d3bc2a
Cocoagoat <denis021299@gmail.com>
2024-10-14 22:27:55 +0300

    Updated setup.py

--- malstats/main/modules/setup.py
+++ malstats/main/modules/setup.py
@@ -4,6 +4,6 @@ import numpy
 
 # Define the Cython extension module
 setup(
-    ext_modules=cythonize("your_module.pyx"),  # Replace with your .pyx file
-    include_dirs=[numpy.get_include()],        # Add NumPy headers for compilation
+    ext_modules=cythonize("gottagofasttest2.pyx"),
+    include_dirs=[numpy.get_include()],
 )
