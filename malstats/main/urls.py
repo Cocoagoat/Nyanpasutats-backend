dbe2bf977d02bb00f79238df20a392eb062dbd28
Cocoagoat <denis021299@gmail.com>
2025-01-19 00:27:34 +0200

    Added views for image sharing

--- malstats/main/urls.py
+++ malstats/main/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+# from .views import TokenObtainView
+
 urlpatterns = [
     path("recs/", views.RecommendationsView.as_view(), name="recs"),
     path("affinity/", views.AffinityFinderView.as_view(), name="affinity"),
@@ -8,5 +12,12 @@ urlpatterns = [
     path("img_urls/", views.get_anime_img_urls, name="img_urls"),
     path('tasks/', views.get_task_data, name='tasks'),
     path('queue_pos/', views.get_queue_position, name='queue_pos'),
-    path('update_img_url/', views.update_image_url_view, name='update_img_url')
+    path('update_img_url/', views.update_image_url_view, name='update_img_url'),
+    path('upload_infographic_img/', views.upload_infographic_image, name='upload_infographic_img'),
+    path('fetch_infographic_img/', views.fetch_infographic_image, name='fetch_infographic_img')
+    # path('api/token/', TokenObtainView.as_view(), name='token_obtain'),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- malstats/main/views.py
+++ malstats/main/views.py
@@ -1,28 +1,53 @@
 import time
 
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.core.files.uploadhandler import TemporaryFileUploadHandler
+from django.shortcuts import get_object_or_404
+from django.core.files.storage import default_storage
+from django.views.decorators.csrf import csrf_exempt
+from django.http.multipartparser import MultiPartParser
 from django.views.decorators.http import require_POST
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.core.cache import cache
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer
-from django.http import JsonResponse
+from django.http import JsonResponse, FileResponse
+from django.http.multipartparser import MultiPartParser
 from .modules.MAL_utils import get_data
 from .modules.general_utils import determine_queue_cache_key
 from .tasks import get_user_seasonal_stats_task, get_user_recs_task, get_user_affs_task
-from .models import AnimeDataUpdated
+from .models import AnimeDataUpdated, SavedImage
 import requests
 import json
 import logging
+import os
 from urllib.parse import unquote
 from celery.result import AsyncResult
 from django.db.utils import OperationalError
-
 
 logger = logging.getLogger('nyanpasutats')
 
 CACHE_TIMEOUT = 3600
 TASK_TIMEOUT = 3600
+
+
+# class TokenObtainView(APIView):
+#     permission_classes = (AllowAny,)
+#
+#     def get(self, request):
+#         # For simplicity, assuming the user is already authenticated
+#         # Here, generate the token for an existing user (e.g., during login)
+#         user = User.objects.get(username=request.data['username'])
+#         refresh = RefreshToken.for_user(user)
+#
+#         return Response({
+#             'refresh': str(refresh),
+#             'access': str(refresh.access_token),
+#         })
+
 
 
 def get_task_data(request):
@@ -95,8 +120,13 @@ def get_anime_img_url(request):
 @api_view(('GET',))
 @renderer_classes((JSONRenderer,))
 def get_queue_position(request):
+    random = request.GET.get('random')
+    if not random:
+        return Response({'error': "Unauthorized access"}, status=403)
+    print("Got past random thingy")
     queue_type = request.GET.get('type')
     queue_cache_key = determine_queue_cache_key(queue_type, "view")
+    print("Returning queuepos")
     return Response({'queuePosition': cache.get(queue_cache_key, 1)}, status=200)
 
 
@@ -149,3 +179,59 @@ def update_image_url_view(request):
         existing_show_data.save()
         return JsonResponse({'added': True})
     return JsonResponse({'added': False})
+
+
+@csrf_exempt
+def upload_infographic_image(request):
+    if request.method == 'POST':
+        print("Entered request")
+        upload_handlers = [TemporaryFileUploadHandler()]
+        parser = MultiPartParser(request.META, request, upload_handlers)
+        parsed_data, files = parser.parse()
+        if 'image' not in files:
+            return JsonResponse({"error": "Image not sent"}, status=400)
+
+        print(f"Parsed data, files : {parsed_data}, {files}")
+        image_type = parsed_data.get('image_type')  # Adjust key as necessary
+        if image_type not in ['tierList', 'seasonalCard']:
+            return JsonResponse({"error": "Invalid image type"}, status=400)
+        print(f"image_type is {image_type}")
+        # image_file = files.get('image')
+        image_name = parsed_data.get('image_name')
+        print(f"image_name is {image_name}")
+        try:
+            file_path = default_storage.save(f"userImages/{image_name}.png", files['image'])
+            # This file_path might not be exactly f"userImages/{image_name}.png if the image
+            # with that name already existed. In that case, a random string will be added to the name.
+            # SomeImage.png ---> SomeImage-34SDMK189.png.
+            saved_image = SavedImage(file_path=file_path, file_name=image_name)
+            saved_image.save()
+        except Exception as e:
+            print(e)
+
+        image_url = f"http://localhost:3000/userImages/{saved_image.unique_id}"
+        print(f"image_url is {image_url}")# Adjust based on your MEDIA_URL
+        return JsonResponse({"url": image_url})
+    else:
+        return JsonResponse({"error": "No image provided"}, status=400)
+
+
+def fetch_infographic_image(request):
+    unique_id = request.GET.get('unique_id', "")
+    print(f"unique_id is {unique_id}")
+    saved_image = get_object_or_404(SavedImage, unique_id=unique_id)
+
+    # Serve the file
+    file_path = saved_image.file_path  # Full path to the file
+    file_url = saved_image.file_path.url
+    image_url = request.build_absolute_uri(f"{settings.MEDIA_URL}{file_path}")
+    print(f"file_path is {file_path}")
+    print(f"file_url is {file_url}")
+    print(f"image_url is {image_url}")
+    try:
+        # return FileResponse(open(file_path, 'rb'), content_type='image/png')
+        return JsonResponse({"url": image_url})
+    except Exception as e:
+        print("Exception when returning url", e)
+
+
